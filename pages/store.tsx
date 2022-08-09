4d832dbd2e735fd3dffe5af8e3109fca7efc7dd0
Ayeman <ayemanbsalauddin@gmail.com>
2022-08-09 19:31:54 +0600

    preparing for new boot

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../src/components/StoreItem";
+import storeItems from "../src/data/items.json";
 
 const store = () => {
-  return <div>store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default store;
